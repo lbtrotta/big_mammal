847973f801e98ef6f2d087fa3f8dc83d6d4416a7
Lauren Trotta <lbtrotta@ufl.edu>
2015-11-13 12:32:05 -0500

    Extant and extinct average mass

--- Assignment 11.R
+++ Assignment 11.R
@@ -1,5 +1,27 @@
+##Link to github
+##https://github.com/lbtrotta/big_mammal.git
+
+library(dplyr)
+
 ##Import data, no column names, and 999 is NA, but so is North America
+##Want to ignore species listed as historical in status column
+mammal_sizes <- read.csv("MOMv3.3.txt", sep="\t", header=FALSE, stringsAsFactors = FALSE, na.strings = "-999")
 
-mammal <- read.csv("MOMv3.3.txt", sep="\t", header=FALSE, stringsAsFactors = FALSE, na.strings = "-999")
+head(mammal_sizes)
 
-head(mammal)
+##Add col names
+colnames(mammal_sizes) <- c("continent", "status", "order", 
+                            "family", "genus", "species", "log_mass", "combined_mass", 
+                            "reference")
+
+##mean mass of extant species
+extant_mass <- mean(mammal_sizes$combined_mass[mammal_sizes$status=="extant"], na.rm=T)
+
+##mean mass of extinct species
+extinct_mass <- mean(mammal_sizes$combined_mass[mammal_sizes$status=="extinct"], na.rm=T)
+
+
+
+
+
+
